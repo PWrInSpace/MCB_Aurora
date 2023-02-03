2cfdb5fb819bd7f8596c2400d090a1911f641059
jsiuda01 <jakkub68@gmail.com>
2023-02-03 13:33:24 +0100

    fix build

--- main/main.c
+++ main/main.c
@@ -5,13 +5,11 @@
 #include "freertos/task.h"
 #include "esp_flash.h"
 #include "esp_log.h"
-#include "test.h"
 
 #define TAG "AURORA"
 
 void app_main(void) {
     ESP_LOGI(TAG, "Hello world!\n");
-    TEST_test(123);
     for (int i = 10; i >= 0; i--) {
         ESP_LOGI(TAG, "Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
