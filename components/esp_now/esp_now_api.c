4e8257df618e2a731030190dbf2e880a5656cb7d
jsiuda01 <jakkub68@gmail.com>
2023-03-02 22:32:50 +0100

    ena task

--- components/esp_now/esp_now_api.c
+++ components/esp_now/esp_now_api.c
@@ -1,66 +1,147 @@
 // Copyright 2022 PWrInSpace, Kuba
 
+#include <memory.h>
+
 #include "esp_now_api.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 #include "freertos/task.h"
 #include "esp_event.h"
+#include "esp_mac.h"
 #include "esp_netif.h"
 #include "esp_wifi.h"
 #include "esp_now.h"
 #include "nvs_flash.h"
+#include "esp_log.h"
+
+#define TAG "ENA"
 
 static struct {
-  ENA_device_t *devices;
-  size_t number_of_devices;
-  ENA_error error_fnc;
+    ENA_device_t *devices;
+    size_t number_of_devices;
+    ENA_on_error error_fnc;
 
-  TaskHandle_t api_task;
-  QueueHandle_t send_queue;
-  uint32_t retakes;
+    ENA_transmit_param_t last_packet;
+    bool is_packet_transmiting;
+
+    TaskHandle_t api_task;  // task for handling callbacks
+    QueueHandle_t transmit_queue;  // queue for transmiting data
+    QueueHandle_t send_cb_queue;  // queue for handling send callback
+    QueueHandle_t receive_cb_queue;  // queue for handling receive callback
 } gb;
 
-static void now_receive_cb(const esp_now_recv_info_t *info, const uint8_t *data, int data_len) {
-    ESP_LOGI(TAG, "Packet received, RSSI %d", info->rx_ctrl->rssi);
-    if (info->src_addr == NULL || data == NULL || data_len == 0) {
+static void now_receive_cb(const uint8_t *mac, const uint8_t *data, int data_len) {
+    ESP_LOGI(TAG, "Packet received, RSSI" MACSTR, MAC2STR(mac));
+    if (mac == NULL || data == NULL || data_len == 0) {
         ESP_LOGE(TAG, "Argument error :C");
+        gb.error_fnc(ENA_REC);
         return;
     }
 
-    if (info->rx_ctrl->rx_state != 0) {
-        ESP_LOGE(TAG, "Packet error :C");
-        return;
+    ENA_receive_cb_t rec_cb_data;
+    memcpy(&rec_cb_data.src_mac, mac, sizeof(MAC_ADRESS_SIZE));
+    memcpy(&rec_cb_data.buffer, data, data_len);
+    rec_cb_data.len = data_len;
+
+    if (xQueueSend(gb.receive_cb_queue, &rec_cb_data, 0) != pdTRUE) {
+        ESP_LOGE(TAG, "Unavle to add receive callback to queue");
+        gb.error_fnc(ENA_REC_CB_QUEUE);
     }
-
-
 }
 
 static void now_send_cb(const uint8_t *mac_addres, esp_now_send_status_t status) {
-    if (status == ESP_NOW_SEND_SUCCESS) {
+    ENA_send_cb_t info;
 
-    } else {
+    info.status = status;
+    memcpy(&info.mac, mac_addres, MAC_ADRESS_SIZE);
 
+    if (xQueueSend(gb.send_cb_queue, &info, 0) != pdTRUE){
+        ESP_LOGE(TAG, "Send callback queeu error");
+        gb.error_fnc(ENA_SEND_CB_QUEUE);
     }
 }
 
-esp_err_t ENA_init(uint8_t *mac_addres) {
-  esp_base_mac_addr_set(mac_address);
 
-  // wifi setup
-  nvs_flash_init();
-  esp_netif_init();
-  esp_event_loop_create_default();
-  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  esp_wifi_init(&cfg);
-  esp_wifi_set_storage(WIFI_STORAGE_RAM);
-  esp_wifi_set_mode(WIFI_MODE_STA);
-  esp_wifi_start();
+static bool address_compare(const uint8_t *addr1, const uint8_t *addr2) {
+    for (size_t i = 0; i < MAC_ADRESS_SIZE; ++i) {
+        if (addr1[i] != addr2[i]) {
+            return false;
+        }
+    }
 
-  esp_now_init();
+    return true;
+}
 
-  esp_now_register_recv_cb(now_receive_cb);
-  esp_now_register_send_cb(now_send_cb);
+static void send_last_packet(void) {
+    if (esp_now_send(gb.last_packet.mac, gb.last_packet.buffer, gb.last_packet.len) != ESP_OK) {
+        ESP_LOGE(TAG, "Send error");
+        gb.error_fnc(ENA_SEND);
+    }
+}
+
+static void now_task(void *arg) {
+    ENA_receive_cb_t rec_cb_data;
+    ENA_send_cb_t send_cb_data;
+
+    while (1) {
+        if (xQueueReceive(gb.receive_cb_queue, &rec_cb_data, 10) == pdTRUE) {
+            for (size_t i = 0; i < gb.number_of_devices; ++i) {
+                if (address_compare(gb.devices[i].mac, rec_cb_data.src_mac) == true) {
+                    gb.devices[i].on_receive(rec_cb_data.buffer, rec_cb_data.len);
+                    break;
+                }
+            }
+        }
+
+        if (xQueueReceive(gb.send_cb_queue, &send_cb_data, 10) == pdTRUE) {
+            if (send_cb_data.status == ESP_NOW_SEND_SUCCESS) {
+                // there is more data to transmit
+                if (xQueueReceive(gb.transmit_queue, &gb.last_packet, 0) == pdTRUE) {
+                    send_last_packet();  // clean or not :W
+                } else {
+                    gb.is_packet_transmiting = false;
+                }
+            } else {
+                if (gb.last_packet.count != 0) {
+                    gb.last_packet.count -= 1;
+                    send_last_packet();
+                } else {
+                    gb.error_fnc(ENA_SEND_REPLY);
+                    if (xQueueReceive(gb.transmit_queue, &gb.last_packet, 0) == pdTRUE) {
+                        send_last_packet();
+                    } else {
+                        gb.is_packet_transmiting = false;
+                    }
+                }
+            }
+        }
+    }
+}
+
+esp_err_t ENA_init(uint8_t *mac_address) {
+    esp_base_mac_addr_set(mac_address);
+
+    // wifi setup
+    nvs_flash_init();
+    esp_netif_init();
+    esp_event_loop_create_default();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+    esp_wifi_set_storage(WIFI_STORAGE_RAM);
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_start();
+
+    esp_now_init();
+
+    esp_now_register_recv_cb(now_receive_cb);
+    esp_now_register_send_cb(now_send_cb);
+
+    gb.transmit_queue = xQueueCreate(5, sizeof(ENA_transmit_param_t));
+    gb.send_cb_queue = xQueueCreate(5, sizeof(ENA_send_cb_t));
+    gb.receive_cb_queue = xQueueCreate(5, sizeof(ENA_receive_cb_t));
+
+    return ESP_OK;
 }
 
 esp_err_t ENA_register_devices(ENA_device_t *devices, size_t number_of_devices) {
@@ -73,7 +154,7 @@ esp_err_t ENA_register_devices(ENA_device_t *devices, size_t number_of_devices) 
   return ESP_OK;
 }
 
-esp_err_t ENA_register_error_handler(ENA_error error_fnc) {
+esp_err_t ENA_register_error_handler(ENA_on_error error_fnc) {
   assert(error_fnc == NULL);
 
   gb.error_fnc = error_fnc;
@@ -82,4 +163,32 @@ esp_err_t ENA_register_error_handler(ENA_error error_fnc) {
 }
 
 // esp_err_t ENA_run(void);
-esp_err_t ENA_send(ENA_device_t *device, void *data, size_t data_size);
+esp_err_t ENA_send(ENA_device_t *device, void *data, size_t data_size, uint8_t retakes) {
+    assert(data == NULL);
+    assert(data_size == 0);
+    ENA_transmit_param_t send_param;
+
+    if (device == NULL) {
+        send_param.broadcast = true;
+    } else {
+        send_param.broadcast = false;
+    }
+
+    memcpy(&send_param.mac, &device->mac, MAC_ADRESS_SIZE);
+    memcpy(send_param.buffer, data, data_size);
+    send_param.len = data_size;
+    send_param.count = retakes;
+
+
+    if (gb.is_packet_transmiting == true) {
+        if (xQueueSend(gb.transmit_queue, &send_param, 10) == pdFALSE) {
+            return ESP_FAIL;
+        }
+    } else {
+        gb.last_packet = send_param;
+        send_last_packet();
+        gb.is_packet_transmiting = true;
+    }
+
+    return ESP_OK;
+}
--- components/esp_now/esp_now_api.h
+++ components/esp_now/esp_now_api.h
@@ -5,27 +5,51 @@
 
 #include "esp_now.h"
 
-typedef void(*ENA_on_receive)(void *data);
-typedef void(*ENA_error)(void);
+#define MAC_ADRESS_SIZE 6
+#define MAX_DATA_SIZE 250
+
+typedef enum {
+    ENA_OK = 0,
+    ENA_REC,
+    ENA_SEND,
+    ENA_SEND_REPLY,
+    ENA_REC_CB_QUEUE,
+    ENA_SEND_CB_QUEUE,
+} ENA_ERROR;
+
+typedef void(*ENA_on_receive)(uint8_t *data, size_t len);
+typedef void(*ENA_on_error)(ENA_ERROR error);
 
 typedef struct {
-    char mac[6];
+    uint8_t mac[6];
     // char id;
     ENA_on_receive on_receive;
 } ENA_device_t;
 
 typedef struct {
     bool broadcast;
-    uint8_t *dest_mac;
+    uint8_t mac[MAC_ADRESS_SIZE];
     uint8_t count;
-    uint8_t *buffer;
+    uint8_t buffer[MAX_DATA_SIZE];
     size_t len;
-} ENA_send_param_t
+} ENA_transmit_param_t;
 
-esp_err_t ENA_init(uint32_t priority, uint32_t depth);
+
+typedef struct {
+    esp_now_send_status_t status;
+    uint8_t mac[MAC_ADRESS_SIZE];
+} ENA_send_cb_t;
+
+typedef struct {
+    uint8_t src_mac[MAC_ADRESS_SIZE];
+    uint8_t buffer[MAX_DATA_SIZE];
+    size_t len;
+} ENA_receive_cb_t;
+
+esp_err_t ENA_init(uint8_t *mac_address);
 esp_err_t ENA_register_devices(ENA_device_t *devices, size_t number_of_devices);
-esp_err_t ENA_register_error_handler(ENA_error error_fnc);
+esp_err_t ENA_register_error_handler(ENA_on_error error_fnc);
 // esp_err_t ENA_run(void);
-esp_err_t ENA_send(ENA_device_t *device, void *data, size_t data_size);
+esp_err_t ENA_send(ENA_device_t *device, void *data, size_t data_size, uint8_t retakes);
 // esp_err_t ENA_send_by_id(char id, void *data, size_t data_size);
 #endif
