5c2d8f8023b90b96cd25d8bbfae9a87d6d94b67a
jsiuda01 <jakkub68@gmail.com>
2023-03-23 18:50:01 +0100

    change to lora frame format, remove task

--- components/commands/commands.c
+++ components/commands/commands.c
@@ -7,18 +7,18 @@
 #define TAG "CMD"
 
 static struct {
-    TaskHandle_t task;
-    QueueHandle_t receive_queue;
+    // TaskHandle_t task;
+    // QueueHandle_t receive_queue;
 
-    command_handle_t *registered_commands;
-    size_t number_of_commands;
+    rf_cmd_device_t *devices;
+    size_t number_of_devices;
 
-    cmd_on_task_error error_task_fnc;
+    rf_cmd_on_task_error error_task_fnc;
 } gb = {
-    .task = NULL,
-    .receive_queue = NULL,
-    .registered_commands = NULL,
-    .number_of_commands = 0,
+    // .task = NULL,
+    // .receive_queue = NULL,
+    .devices = NULL,
+    .number_of_devices = 0,
     .error_task_fnc = NULL,
 };
 
@@ -29,112 +29,127 @@ static void report_error(COMMAND_ERROR error) {
     }
 }
 
-static bool find_command_and_execute(command_message_t *received_command) {
-    for (size_t i = 0; i < gb.number_of_commands; ++i) {
-        if (gb.registered_commands[i].command == received_command->cmd.command) {
-            if (gb.registered_commands[i].receive_fnc != NULL) {
-                gb.registered_commands[i].receive_fnc(
+
+static bool get_device_index(rf_cmd_device_id_t dev_id, size_t *dev_index) {
+    for (size_t i = 0; gb.number_of_devices; ++i) {
+        if (gb.devices[i].dev_id == dev_id) {
+            *dev_index = i;
+            return true;
+        }
+    }
+    return false;
+}
+
+static bool find_command_and_execute(size_t dev_index, rf_cmd_message_t *received_command) {
+    for (size_t i = 0; i < gb.devices[dev_index].number_of_commands; ++i) {
+        if (gb.devices->commands[i].command_id == received_command->cmd.command) {
+            if (gb.devices->commands[i].on_command_receive_fnc != NULL) {
+                gb.devices->commands[i].on_command_receive_fnc(
                         received_command->cmd.command, received_command->cmd.payload);
             }
             return true;
         }
     }
 
-    if (gb.task != NULL) {
-        report_error(COMMAND_NOT_FOUND);
-    }
+    // if (gb.task != NULL) {
+    //     report_error(COMMAND_NOT_FOUND);
+    // }
 
     return false;
 }
 
-static void command_task(void *arg) {
-    command_message_t received_command;
-    while (1) {
-        if (xQueueReceive(gb.receive_queue, &received_command, 0) == pdTRUE) {
-            if (gb.registered_commands != NULL) {
-                find_command_and_execute(&received_command);
-            }
-        }
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
-}
+// static void command_task(void *arg) {
+//     rf_cmd_message_t received_command;
+//     while (1) {
+//         if (xQueueReceive(gb.receive_queue, &received_command, 0) == pdTRUE) {
+//             if (gb.registered_commands != NULL) {
+//                 find_command_and_execute(&received_command);
+//             }
+//         }
+//         vTaskDelay(pdMS_TO_TICKS(10));
+//     }
+// }
 
 
-static bool register_commands_and_err_fnc(command_config_t *cfg) {
-    assert(cfg->commands != NULL);
-    assert(cfg->number_of_commands > 0);
-    if (cfg->commands == NULL || cfg->number_of_commands == 0) {
+static bool register_commands_and_err_fnc(rf_cmd_config_t *cfg) {
+    assert(cfg->devices != NULL);
+    assert(cfg->number_of_devices > 0);
+    if (cfg->devices == NULL || cfg->number_of_devices == 0) {
         return false;
     }
 
-    gb.registered_commands = cfg->commands;
-    gb.number_of_commands = cfg->number_of_commands;
+    gb.devices = cfg->devices;
+    gb.number_of_devices = cfg->number_of_devices;
     gb.error_task_fnc = cfg->task_error_fnc;
 
     return true;
 }
 
-bool CMD_init(command_config_t *cfg) {
+bool rf_cmd_init(rf_cmd_config_t *cfg) {
     return register_commands_and_err_fnc(cfg);
 }
 
-bool CMD_init_with_task(command_config_t *cfg) {
-    if (gb.task != NULL) {
+// bool rf_cmd_init_with_task(rf_cmd_config_t *cfg) {
+//     if (gb.task != NULL) {
+//         return false;
+//     }
+
+//     if (register_commands_and_err_fnc(cfg) == false) {
+//         return false;
+//     }
+
+//     gb.receive_queue = xQueueCreate(cfg->queue_size, sizeof(rf_cmd_message_t));
+//     if (gb.receive_queue == NULL) {
+//         return false;
+//     }
+
+//     xTaskCreatePinnedToCore(
+//         command_task,
+//         "Command task",
+//         cfg->stack_depth,
+//         NULL,
+//         cfg->priority,
+//         &gb.task,
+//         cfg->core_id);
+
+//     if (gb.task == NULL) {
+//         vQueueDelete(gb.receive_queue);
+//         gb.receive_queue = NULL;
+//         return false;
+//     }
+
+//     return true;
+// }
+
+// bool rf_cmd_send_command_for_processing(rf_cmd_message_t *command) {
+//     assert(command != NULL);
+//     assert(gb.receive_queue != NULL);
+//     if (command == NULL) {
+//         return false;
+//     }
+
+//     if (gb.receive_queue == NULL) {
+//         return false;
+//     }
+
+//     if (xQueueSend(gb.receive_queue, command, 0) != pdTRUE) {
+//         ESP_LOGE(TAG, "Unable to add to queue");
+//         return false;
+//     }
+
+//     return true;
+// }
+
+bool rf_cmd_process_command(rf_cmd_device_id_t dev_id, rf_cmd_message_t *command) {
+    size_t index = 0;
+    if (get_device_index(dev_id, &index) == false) {
         return false;
     }
-
-    if (register_commands_and_err_fnc(cfg) == false) {
-        return false;
-    }
-
-    gb.receive_queue = xQueueCreate(cfg->queue_size, sizeof(command_message_t));
-    if (gb.receive_queue == NULL) {
-        return false;
-    }
-
-    xTaskCreatePinnedToCore(
-        command_task,
-        "Command task",
-        cfg->stack_depth,
-        NULL,
-        cfg->priority,
-        &gb.task,
-        cfg->core_id);
-
-    if (gb.task == NULL) {
-        vQueueDelete(gb.receive_queue);
-        gb.receive_queue = NULL;
-        return false;
-    }
-
-    return true;
+    return find_command_and_execute(index ,command);
 }
 
-bool CMD_send_command_for_processing(command_message_t *command) {
-    assert(command != NULL);
-    assert(gb.receive_queue != NULL);
-    if (command == NULL) {
-        return false;
-    }
-
-    if (gb.receive_queue == NULL) {
-        return false;
-    }
-
-    if (xQueueSend(gb.receive_queue, command, 0) != pdTRUE) {
-        ESP_LOGE(TAG, "Unable to add to queue");
-        return false;
-    }
-
-    return true;
-}
-
-bool CMD_process_command(command_message_t *command) {
-    return find_command_and_execute(command);
-}
-
-command_message_t CMD_create_message(uint32_t command, int32_t payload) {
-    command_message_t message = {
+rf_cmd_message_t rf_cmd_create_message(uint32_t command, int32_t payload) {
+    rf_cmd_message_t message = {
         .cmd.command = command,
         .cmd.payload = payload
     };
@@ -142,15 +157,15 @@ command_message_t CMD_create_message(uint32_t command, int32_t payload) {
     return message;
 }
 
-void CMD_terminate_task(void) {
-    if (gb.receive_queue != NULL) {
-        vQueueDelete(gb.receive_queue);
-        gb.receive_queue = NULL;
-    }
+// void rf_cmd_terminate_task(void) {
+//     if (gb.receive_queue != NULL) {
+//         vQueueDelete(gb.receive_queue);
+//         gb.receive_queue = NULL;
+//     }
 
-    if (gb.task != NULL) {
-        vTaskDelete(gb.task);
-        gb.task = NULL;
-    }
-}
+//     if (gb.task != NULL) {
+//         vTaskDelete(gb.task);
+//         gb.task = NULL;
+//     }
+// }
 
--- components/commands/commands.h
+++ components/commands/commands.h
@@ -18,13 +18,22 @@ typedef enum {
     COMMAND_NOT_FOUND = 1,
 } COMMAND_ERROR;
 
-typedef void (*cmd_on_command_receive)(uint32_t command, int32_t payload);
-typedef void (*cmd_on_task_error)(COMMAND_ERROR error);
+typedef uint8_t rf_cmd_device_id_t;
+typedef uint32_t rf_cmd_command_id_t;
+typedef void (*rf_cmd_on_command_receive)(uint32_t command, int32_t payload, bool sudo);
+typedef void (*rf_cmd_on_task_error)(COMMAND_ERROR error);
+
 
 typedef struct {
-    uint32_t command;
-    cmd_on_command_receive receive_fnc;
-} command_handle_t;
+    rf_cmd_command_id_t command_id;
+    rf_cmd_on_command_receive on_command_receive_fnc;
+} rf_cmd_command_t;
+
+typedef struct {
+    rf_cmd_device_id_t dev_id;
+    rf_cmd_command_t *commands;
+    size_t number_of_commands;
+} rf_cmd_device_t;
 
 typedef union {
     struct command {
@@ -33,19 +42,20 @@ typedef union {
     } cmd;
 
     uint8_t raw[sizeof(struct command)];
-} command_message_t;
+} rf_cmd_message_t;
+
 
 typedef struct {
-    command_handle_t *commands;  // pointer to static allocated array of commands
-    size_t number_of_commands;   // numbner of commands
+    rf_cmd_device_t *devices;  // pointer to static allocated array of commands
+    size_t number_of_devices;   // numbner of commands
     // function calls on error, can be NULL if you dont want to use
-    cmd_on_task_error task_error_fnc;
+    rf_cmd_on_task_error task_error_fnc;
 
-    uint32_t stack_depth;   // task stack_depth
-    BaseType_t core_id;     // task core_id
-    UBaseType_t priority;   // task priority
-    uint8_t queue_size;     // queueu size
-} command_config_t;
+    // uint32_t stack_depth;   // task stack_depth
+    // BaseType_t core_id;     // task core_id
+    // UBaseType_t priority;   // task priority
+    // uint8_t queue_size;     // queueu size
+} rf_cmd_config_t;
 
 /**
  * @brief Initialize commands api without task
@@ -54,50 +64,50 @@ typedef struct {
  * @return true :)
  * @return false :C
  */
-bool CMD_init(command_config_t *cfg);
+bool rf_cmd_init(rf_cmd_config_t *cfg);
 
 /**
- * @brief Initialize command task
+ * @brief Initialize rf_cmd task
  *
  * @param cfg pointer to config
  * @return true :)
  * @return false :C
  */
-bool CMD_init_with_task(command_config_t *cfg);
+bool rf_cmd_init_with_task(rf_cmd_config_t *cfg);
 
 
 /**
- * @brief Send command to task for processing
+ * @brief Send rf_cmd to task for processing
  *
- * @param command pointer to command
+ * @param rf_cmd pointer to rf_cmd
  * @return true :D
  * @return false :C
  */
-bool CMD_send_command_for_processing(command_message_t *command);
+bool rf_cmd_send_rf_cmd_for_processing(rf_cmd_message_t *command);
 
 /**
- * @brief Process command without task
+ * @brief Process rf_cmd without task
  * 
- * @param command pointer to command
+ * @param rf_cmd pointer to rf_cmd
  * @return true :D
  * @return false :C
  */
-bool CMD_process_command(command_message_t *command);
+bool rf_cmd_process_rf_cmd(rf_cmd_message_t *command);
 
 /**
  * @brief Creater message
  *
- * @param command command
+ * @param rf_cmd rf_cmd
  * @param payload payload
- * @return command_message_t created message
+ * @return rf_cmd_message_t created message
  */
-command_message_t CMD_create_message(uint32_t command, int32_t payload);
+rf_cmd_message_t rf_cmd_create_message(uint32_t command, int32_t payload);
 
 /**
  * @brief Terminate task
  *
  */
-void CMD_terminate_task(void);
+void rf_cmd_terminate_task(void);
 
 #ifdef __cplusplus
 }
