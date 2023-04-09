f3735aa82e8f4d461e270bfb8a6016071b7468bb
jsiuda01 <jakkub68@gmail.com>
2023-04-09 22:42:39 +0200

    protobuf frame

--- components/protobuf/data_to_protobuf.c
+++ components/protobuf/data_to_protobuf.c
@@ -1,4 +1,8 @@
 #include "slave_structs.h"
 #include "lora.pb-c.h"
+#include "rocket_data.h"
 
-// void create_frame() 
+// void create_porotobuf_frame() {
+//     rocket_data_t data = rocket_data_get();
+
+// }
