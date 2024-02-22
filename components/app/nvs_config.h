66eaccbce3653a718b6e29e8d7baade1c5278af5
Matteo <matteotennisboy@gmail.com>
2024-02-22 23:42:55 +0100

    added nvs_config

---
+++ components/app/nvs_config.h
@@ -0,0 +1,19 @@
+#ifndef NVS_CONFIGWS
+#define NVS_CONFIG
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <flash_nvs.h>
+
+#define KEY1 "klucz1"
+#define KEY1_DATA 123
+#define DATA_LENGTH 1
+NVSData key_value[] = 
+{
+    {KEY1, KEY1_DATA},
+
+};
+
+NVSResult res = NVS_init(&key_value,DATA_LENGTH);
+
+#endif
